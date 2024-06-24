aac9aa36a1463b0448b7834ac9e6b32abad3bae8
Marcos Veniciu de sa Barbalho <marcos.barbalho@ufv.br>
2024-06-24 10:37:57 -0300

    Download gguf

--- hugfacing_donwload.py
+++ hugfacing_donwload.py
@@ -1,5 +1,3 @@
 from huggingface_hub import hf_hub_download
 
-hf_hub_download(repo_id="Sparcos/TinyLlama_v1.1-qlora-finetunined-UFV_GGUF",
-                filename="FP16.gguf",
-                local_dir="./model")
+hf_hub_download(repo_id="Sparcos/TinyLlama_v1.1-qlora-finetunined-UFV_GGUF", filename="FP16.gguf", local_dir="./model")
