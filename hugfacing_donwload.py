0199724719fdd90cbd2c5c79ea8e2a4df124fad4
Marcos Veniciu de sa Barbalho <marcos.barbalho@ufv.br>
2024-06-24 11:01:28 -0300

    tfa

--- hugfacing_donwload.py
+++ hugfacing_donwload.py
@@ -1,9 +1,9 @@
 from huggingface_hub import hf_hub_download
 import os
 
-id = "Sparcos/TinyLlama_v1.1-qlora-finetunined-UFV_GGUF"
+repo_id = "Sparcos/TinyLlama_v1.1-qlora-finetunined-UFV_GGUF"
 local_dir = "./model"
-arquivo = "FP16.gguf"
+filename = "FP16.gguf"
 
 if not os.path.exists(local_dir):
     os.makedirs(local_dir)
@@ -18,8 +18,8 @@ if not os.path.exists(temp_dir):
 # Defina a variável de ambiente para usar este diretório temporário
 os.environ["HF_HOME"] = temp_dir
 
+file_path = hf_hub_download(repo_id=repo_id, filename=filename, cache_dir=temp_dir)
 
-
-hf_hub_download(repo_id=id, filename=arquivo)
-
+# Move the downloaded file to the local directory
+os.rename(file_path, os.path.join(local_dir, filename))
 print(f"File downloaded to: {local_dir}")
