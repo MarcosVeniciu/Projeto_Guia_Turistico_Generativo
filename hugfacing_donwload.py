b6ae75de1e0b70a8c13f9adbdd2a7518c79143a7
Marcos Veniciu de sa Barbalho <marcos.barbalho@ufv.br>
2024-06-24 10:44:42 -0300

    dowmload gguf

--- hugfacing_donwload.py
+++ hugfacing_donwload.py
@@ -1,3 +1,25 @@
 from huggingface_hub import hf_hub_download
+import os
 
-hf_hub_download(repo_id="Sparcos/TinyLlama_v1.1-qlora-finetunined-UFV_GGUF", filename="FP16.gguf")#, local_dir="./model")
+id = "Sparcos/TinyLlama_v1.1-qlora-finetunined-UFV_GGUF"
+local_dir = "./model"
+arquivo = "FP16.gguf"
+
+if not os.path.exists(local_dir):
+    os.makedirs(local_dir)
+
+# Define a pasta temporária com espaço suficiente
+temp_dir = "./temp_dir"  # Substitua pelo caminho de um diretório com espaço suficiente
+
+# Certifique-se de que o diretório temporário existe
+if not os.path.exists(temp_dir):
+    os.makedirs(temp_dir)
+
+# Defina a variável de ambiente para usar este diretório temporário
+os.environ["HF_HOME"] = temp_dir
+
+
+
+hf_hub_download(repo_id=id, filename=arquivo, local_dir=local_dir)
+
+print(f"File downloaded to: {local_dir}")
